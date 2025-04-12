4685e5723819727cadcd98b5f13858d0a5ff95ec
Akash Verma <akash.verma.5209@gmail.com>
2025-04-12 16:07:56 +0530

    Add files via upload

---
+++ machineCoding/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import  Todo from './components/Todo'
+import {StopWatch  } from './components/StopWatch'
+
+function App() {
+  
+
+  return (
+   <>
+    <StopWatch/>
+     
+   </>
+   
+ 
+ 
+
+  )
+}
+
+export default App
